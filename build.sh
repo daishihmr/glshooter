387a93ff52a114e063779ec6587e9ac76b94b4ba
daishihmr <daishi.hmr@gmail.com>
2013-01-08 16:27:42 +0900

    build tool

---
+++ build.sh
@@ -0,0 +1,42 @@
+java -jar compiler.jar --js_output_file glshooter.min.js \
+    --language_in ECMASCRIPT5 \
+    --compilation_level ADVANCED_OPTIMIZATIONS \
+    --externs lib/tmlib.js \
+    --externs lib/gl-matrix.js \
+    --externs lib/bulletml.js \
+    --js mylib/bulletml.gl2d.js \
+    --js mylib/scene.js \
+    --js mylib/sprite.js \
+    --js mylib/util.js \
+    --js game/background.js \
+    --js game/bomb.js \
+    --js game/boss.js \
+    --js game/explosion.js \
+    --js game/labels.js \
+    --js game/player.js \
+    --js game/scenes.js \
+    --js game/patterns.js \
+    --js game/enemy-data.js \
+    --js game/stage-data.js \
+    --js game/main.js
+
+cp index.html index-backup.html
+cp index-deploy.html index.html
+
+zip glshooter.zip \
+    index.html \
+    sounds/se_maoudamashii_explosion05.mp3 \
+    sounds/voice_extend.mp3 \
+    sounds/voice_gen-bomb.mp3 \
+    sounds/effect0.mp3 \
+    sounds/nc17909.mp3 \
+    images/boss1.png \
+    images/texture0.png \
+    images/boss2.png \
+    images/boss3.png \
+    lib/gl-matrix-min.js \
+    lib/bulletml.js \
+    lib/tmlib.js \
+    glshooter.min.js
+
+cp index-backup.html index.html
